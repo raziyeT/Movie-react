b9cf414d4b5e98cfb8262c4d5a3746cd838e4fd1
Raziyeh Sadat Takmili <r.takmili@ebcom.ir>
2021-12-11 10:01:47 +0330

    add route to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import './App.css';
-import Movies from './Components/Movies'
-import React, { Component } from 'react';
-import 'font-awesome/css/font-awesome.min.css';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Movies from "./Components/Movies";
+import NavBar from "./Components/NavBar";
+import MovieForm from "./Components/MovieForm";
+import NotFound from "./Components/NotFound";
+import Rentals from "./Components/Rentals";
+import Customers from "./Components/Customers";
+import "font-awesome/css/font-awesome.min.css";
+import "./App.css";
 
 class App extends Component {
-  render(){
+  render() {
     return (
+      <React.Fragment>
+      <NavBar/>
       <main className="container">
-    <Movies/>
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}/>
+          <Route path="/movies" component={Movies} />
+          <Route path="/movieForm" component={MovieForm} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/rentals" component={Rentals} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
+      </React.Fragment>
     );
   }
- 
 }
 
 export default App;
+
+//return(
+//   <div>
+//   <NavBar/>
+//   <div className={content}
+//   <Route path="/Products" component={Product}/>
+//   <Route path="/Posts" component={Posts}/>
+//   <Route path="/admin" component={Dashboard}/>
+//   <Route path="/" exact    component={Home}/>
+//   </div>
+//);
