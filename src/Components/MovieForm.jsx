dbfbcaac62a990fd9dd04e3b821c350a9d1c59b1
Raziyeh Sadat Takmili <r.takmili@ebcom.ir>
2021-12-13 13:04:56 +0330

    add movieForm component

--- src/Components/MovieForm.jsx
+++ src/Components/MovieForm.jsx
@@ -1,16 +1,78 @@
+import Joi from "joi-browser";
 import React, { Component } from "react";
+import { saveMovie } from "../services/fakeMovieService";
+import { getMovies } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
+import Select from "./Common/Select";
+import Form from "./Common/Form";
 
-const MovieForm = (props) => {
-  return (
-    <div>
-      <h2>MovieForm {props.match.params.id} </h2>
-      <button
-        className="btn btn-primary"
-        onClick={() => props.history.push("/movies")}
-      >
-        Save
-      </button>
-    </div>
-  );
-};
+class MovieForm extends Form {
+  state = {
+    data: {
+      title: "",
+      genreId: "",
+      numberInStock: "",
+      dailyRentalRate: "",
+    },
+    genres: [],
+    errors: {},
+  };
+  schema = {
+    _id: Joi.string(),
+    title: Joi.string().required().label("Title"),
+    genreId: Joi.string().required().label("Genre"),
+    numberInStock: Joi.number()
+      .required()
+      .min(0)
+      .max(100)
+      .label("Number in Stock"),
+    dailyRentalRate: Joi.number()
+      .required()
+      .min(0)
+      .max(10)
+      .label("daily rental rate"),
+  };
+
+  componentDidMount() {
+    const genres = getGenres();
+    this.setState({ genres });
+
+    const movieId = this.props.match.params.id;
+    if (movieId === "new") return;
+
+    const movie = getMovies(movieId);
+    if (!movie) return this.props.history.replace("/not-found");
+
+    this.setState({ data: this.mapToViewModel(movie) });
+  }
+  mapToViewModel = (movie) => {
+    return {
+      _id: movie._id,
+      ttile: movie.title,
+      genreId: movie.genre._id,
+      numberInStock: movie.numberInStock,
+      dailyRentalRate: movie.dailyRentalRate,
+    };
+  };
+  doSubmit = () => {
+    saveMovie(this.state.data);
+    this.props.history.push("/movies");
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Movie Form</h2>
+        {/* {props.match.params.id} */}
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("title", "Title")}
+          {this.renderSelect("genreId", "Genre", this.state.genres)}
+          {this.renderInput("numberInStock", "Number In Stock", "number")}
+          {this.renderInput("dailyRentalRate", "Rate")}
+          {this.renderButton("save")}
+        </form>
+      </div>
+    );
+  }
+}
 export default MovieForm;
